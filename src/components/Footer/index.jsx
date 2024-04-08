3e2298bfff5436b8a3dc654a14bf774726bddbfe
Ghazala Zameer <134362001+ghazalazameer@users.noreply.github.com>
2024-04-08 20:34:51 +0530

    Updated footer responsiveness

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -11,30 +11,32 @@ export default function Footer() {
 </div>
       <footer className="bg-blue-100 py-4 mt-11">
         <div className="flex flex-col items-center sm:flex-row sm:items-start">
-          <div className='ml-6 mt-20'>
-            <img src="src/assets/logo.png" alt="" className="w-32 ml-10 sm:ml-0" />
-            <p className="ml-10 text-gray-500 mt-6 sm:ml-0">Nam posuere accumsan porta. Integer <br />id tincidunt sit amet sed libero.</p>
-          </div>
+        <div className='ml-6 mt-16'>
+  <img src="src/assets/logo.png" alt="" className="w-32 ml-10 sm:ml-0 mt-8" />
+  <p className="ml-10 text-gray-500 mt-2 sm:ml-0 text-sm">
+    Nam posuere accumsan porta. Integer <br />id tincidunt sit amet sed libero.
+  </p>
+</div>
           <div className="foo_section flex flex-col items-center mt-28 sm:flex-row sm:justify-center sm:mt-[90px] sm:space-x-7 sm:ml-14">
   <ul className="mb-4 sm:mb-0 mr-7">
     <li className="first mb-2">COMPANY</li>
-    <li className="text-gray-500">Donec dignissim</li>
-    <li className="text-gray-500">Curabitur egestas</li>
-    <li className="text-gray-500">Nam posuere</li>
-    <li className="text-gray-500">Aenean facilisis</li>
+    <li className="text-gray-500 text-sm">Donec dignissim</li>
+    <li className="text-gray-500 text-sm">Curabitur egestas</li>
+    <li className="text-gray-500 text-sm">Nam posuere</li>
+    <li className="text-gray-500 text-sm">Aenean facilisis</li>
   </ul>
   <ul className="mb-4 sm:mb-0">
     <li className="first mb-2">SERVICES</li>
-    <li className="text-gray-500">Cras convallis</li>
-    <li className="text-gray-500">Vestibulum faucibus</li>
-    <li className="text-gray-500">Quisque lacinia purus</li>
-    <li className="text-gray-500">Aliquam nec ex</li>
+    <li className="text-gray-500 text-sm">Cras convallis</li>
+    <li className="text-gray-500 text-sm">Vestibulum faucibus</li>
+    <li className="text-gray-500 text-sm">Quisque lacinia purus</li>
+    <li className="text-gray-500 text-sm">Aliquam nec ex</li>
   </ul>
   <ul className="mb-6">
     <li className="first mb-2">RESOURCES</li>
-    <li className="text-gray-500">Suspendisse porttitor</li>
-    <li className="text-gray-500">Nam posuere</li>
-    <li className="text-gray-500">Curabitur egestas</li>
+    <li className="text-gray-500 text-sm">Suspendisse porttitor</li>
+    <li className="text-gray-500 text-sm">Nam posuere</li>
+    <li className="text-gray-500 text-sm">Curabitur egestas</li>
   </ul>
 </div>
         </div>
@@ -46,16 +48,18 @@ export default function Footer() {
     <img src="src/assets/Group 9 Copy 3.png" alt="" />
   </div>
 </div>
-        <div className="mr-20 mt-4 flex justify-end">
-          <select id="lang" name="lang" className="ml-auto w-48 border border-teal-400">
-            <option value="English">English</option>
-            <option value="hindi">Hindi</option>
-          </select>
-        </div>
-        <div className="flex justify-end">
-          <img src="src/assets/Plant.png" alt="" className="w-56 h-40 mt-[-7px] mr-16 sm:mr-0" />
-        </div>
-      </footer>
+<div className="footer-section">
+    <div className="mr-20 mt-4 flex justify-end">
+      <select id="lang" name="lang" className="ml-auto w-48 border border-teal-400">
+        <option value="English">English</option>
+        <option value="hindi">Hindi</option>
+      </select>
+    </div>
+    <div className="plant flex justify-end">
+      <img src="src/assets/Plant.png" alt="" className="w-56 h-40 mt-[-7px] mr-16 sm:mr-0" />
+    </div>
+  </div>
+</footer>
     </div>
   );
 }
