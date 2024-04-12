2803425da1648e132589d760b9e7e4b87eecadcf
iambharatchavhan <bharatchavhan141@gmail.com>
2024-04-12 17:22:34 +0530

    App and Main Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,69 @@
-import { Outlet } from "react-router-dom";
+
+import React, { useState } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+
 import Header from "./components/Header/Header";
+import Task from "./Pages/Task"
+import Dashboard from "./components/Desktop14/Dashboard/Dashboard";
+import Doctorprofile from "./components/Doctorprofile/Doctorprofile";
+import FindDoctor from "./components/Find_a_doctors_page/data/FindADoctor";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar/Navbar"
+import Home from "./components/Home";
+import Analytics from "./components/Analytics/Analytics";
+import Patients from "./components/Patients/Patients";
+import Setting from "./Pages/Settings";
+import Support from "./components/Support/Support";
+import Schedule from "./components/Schedule/Schedule";
 import Sidebar from "./components/Sidebar/Sidebar";
-import DoctorProfile from "./components/Doctorprofile/Doctorprofile";
-import Navbar from "./samepages/navbar/Navbar";
+
+
 
 function App() {
+  const [isAdmin, setIsAdmin] = useState(true);
+
+
+  
+
   return (
     <>
-      {/* <Header /> */}
-      {/* <Navbar /> */}
-      <div className="flex justify-end">
-        {/* <Sidebar /> */}
-        <Outlet />
-        {/* <div className="border-2 border-red-500 h-[10svh]">
-          <p>Add Common header here</p>
-        </div> */}
-        {/* test push  */}
-        <DoctorProfile />
-      </div>
+      {!isAdmin ? (
+        <>
+          <Navbar />
+          <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/our services" element={<Services />} />
+            <Route path="/find-doctor" element={<FindDoctor />} />
+            <Route path="/doctors-profile" element={<Doctorprofile />} />
+          </Routes>
+          <Footer />
+        </>
+      ) : (
+        <>
+          {/* <AdminDashboard/> */}
+          <Header />
+          <div className="flex justify-end">
+            <Sidebar />
+            <Routes>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/" element={<Navigate to="/dashboard" />} />
+              <Route path="/schedule" element={<Schedule />} />
+              <Route path="/tasks" element={<Task />} />
+              <Route path="/patients" element={<Patients />} />
+              <Route
+                path="/messages"
+                element={<div> Error in the code please update it ASAP </div>}
+              />
+              <Route path="/analytics" element={<Analytics />} />
+              <Route path="/settings" element={<Setting />} />
+              <Route path="/support" element={<Support />} />
+            </Routes>
+          </div>
+        </>
+      )}
     </>
   );
 }
