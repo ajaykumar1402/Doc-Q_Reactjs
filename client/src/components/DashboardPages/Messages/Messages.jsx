ef2f680fa875cbce8f280ff03cacdbe79a971f23
shreyasks482 <ksshreyas482@gmail.com>
2024-04-20 02:50:50 +0530

    added server folder

---
+++ client/src/components/DashboardPages/Messages/Messages.jsx
@@ -0,0 +1,119 @@
+import "./Messages.css";
+import backArrow from "../../../assets/messages/icons/backarrow.svg";
+import ellipses from "../../../assets/messages/icons/ellipses.svg";
+import callerpic from "../../../assets/messages/images/callerpic.svg";
+import userpic from "../../../assets/messages/images/userpic.svg";
+import profilepic from "../../../assets/messages/images/profilepic.svg";
+import MessageSent from "./components/MessageSent";
+import MessageReceived from "./components/MessageReceived";
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import MessageInput from "./components/MessageInput";
+import CallButtons from "./components/CallButtons";
+import React from "react";
+
+const data = [
+  {
+    name: "Damilola Oyin",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+    time: "45 Mins Ago",
+  },
+];
+
+
+
+const Messages = () => {
+  return (
+    <>
+    <div className='w-full mt-[8vh] px-5 md:mt-[5vh] lg:mt-[12vh] bg-[#FAFAFA] h-auto lg:h-full max-w-[80%] md:max-w-[90%] lg:max-w-[85%]'>
+
+      {data.map((info, index) => (
+        <div key={index}>
+          <div className="bg-gray-50 py-5">
+            Messages &gt; <span>{info.name}</span>
+          </div>
+          <div className="messages flex flex-col lg:flex-row lg:gap-4 bg-gray-50">
+            <div className="lg:w-1/3">
+              <div className="bg-[#FFFFFF] mb-1 rounded-[4.27px] flex items-center">
+                <div className="ml-5 mr-2 flex items-center justify-center">
+                  <MdOutlineKeyboardArrowLeft className="text-[#828282]" />
+                </div>
+                <div className="flex items-center">
+                  <img
+                    src={profilepic}
+                    alt="Profile Pic"
+                    style={{ borderRadius: "50%", marginRight: "8px" }}
+                  />
+                  {info.name}
+                </div>
+                <div className="flex-grow flex justify-end items-center mr-8">
+                  <img src={ellipses} alt="ellipses" />
+                </div>
+              </div>
+              <div className="px-5 pb-5 bg-[#FFFFFF] rounded-[4.27px] pt-10">
+                <MessageSent
+                  message={"Hello, how are you doing?"}
+                  time={"09:27am"}
+                />
+                <MessageReceived
+                  message={"Great, when can we have the meeting?"}
+                  status={"Seen"}
+                />
+                <MessageSent
+                  message={"Maybe, now\nWill that be okay?"}
+                  time={"09:28am"}
+                />
+                <MessageReceived
+                  message={"Yeah\nLets have a video call"}
+                  status={"Seen"}
+                />
+                <MessageSent message={"That would be great"} time={"09:28am"} />
+
+                <div className="mt-28">
+                  <MessageInput onSubmit={() => console.log("done")} />
+                </div>
+              </div>
+            </div>
+            <div className="lg:w-2/3 md:w-full flex flex-col">
+            <div className="h-[510px] xs:h-[50%] bg-[#FFFFFF] rounded-[4.27px] flex items-center justify-center relative overflow-hidden">
+                <img
+                  src={"https://s3-alpha-sig.figma.com/img/b278/bdbc/d1fce6962261395faa1cb4bd7ca79fc9?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bkTVb9FgAr9qr9bfpt7vEmSb7rR8K~J2n-NGfLujK5~iBRVjN6zaSHsezfmjq7ulK~0yAAdnWXTowWaym21R-5qZZcnarmWxtgn3rW8iAblowoYZymdihluEnkb-0GL2KNUMYocSgIhESfhIoJjXSHZjuwfAM6iag7yehEwDkoRo19iVDj~uf~GIOKoFMJR902AMRzBVPfI~wAHXt8wfAkqyRrczJJ~tRqu2dpjioIH3FykLidVhaKfP5fjnzcZRBVZSdBNMixv4KEr9Ethu3Pv-rF6fjBCgjZX0Nf52M9GW3TpePXgW~bDun~N9PL6XohSz4AX8dtLN2fBF6MSuRg__"}
+                  alt="Userpic"
+                  className="object-cover"
+                  style={{
+                    borderRadius: "4.27px",
+                    width: "94%",
+                    height: "94%",
+                    // objectFit: "cover",
+                    position: "relative",
+                  }}
+                />
+                <img
+                  src={callerpic}
+                  alt="callerpic"
+                  // className="absolute left-1/2 bottom-10 transform -translate-x-1/2 w-1/3 h-auto"
+
+                  style={{
+                    position: "absolute",
+                    left: "70%",
+                    bottom: "10%",
+                    transform: "translate(-50%, 0)",
+                    width: "30%",
+                    height: "auto",
+                  }}
+                />
+              </div>
+              <div className="callbutton bg-[#FFFFFF] mt-10 py-5 flex justify-center items-center gap-20 rounded-[4.27px]">
+                <CallButtons />
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+      </div>
+    </>
+  );
+};
+
+export default Messages;
+
---
+++ client/src/components/DashboardPages/Patients/Patients.jsx
@@ -0,0 +1,167 @@
+import React from 'react'
+import data from './data.json';
+import './Patients.css';
+import { FaPlus } from "react-icons/fa6";
+import { IoSearch } from "react-icons/io5";
+import { FiFilter } from "react-icons/fi";
+import { BsQuestionCircle } from "react-icons/bs";
+import { Button, IconButton } from "@material-tailwind/react";
+import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
+const Patients = () => {
+
+    const [active, setActive] = React.useState(1);
+    console.log(active);
+    const getItemProps = (index) =>
+    ({
+        variant: active === index ? "filled" : "text",
+        color: "gray", className: "rounded-full  bg-slate-200 w-8 ",
+        onClick: () => setActive(index),
+
+    });
+    const next = () => {
+        if (active === 5) return;
+
+        setActive(active + 1);
+    };
+
+    const prev = () => {
+        if (active === 1) return;
+
+        setActive(active - 1);
+    };
+
+
+    return (
+       
+    <div className='w-full mt-[8vh] px-5 md:mt-[5vh] lg:mt-[12vh] bg-[#FAFAFA] h-auto lg:h-full max-w-[80%] md:max-w-[90%] lg:max-w-[85%]'>
+            <div className="p-4">
+                <span className='font-normal text-sm'>Patient register</span>
+            </div>
+
+            <div className="w-[97.5%] mx-4 my-2 items-center flex justify-between bg-white rounded ">
+                <div className='p-4 items-center'>Total Patients<span className='text-[#828282]'> (487)</span></div>
+                <div className='p-4 items-center flex justify-between'>
+                    <span className="p-2 border-2 border-[#E0E0E0] ">
+                        <FaPlus />
+                    </span>
+                    <span className="p-2  border-2 border-[#E0E0E0] ">
+                        <IoSearch />
+                    </span>
+                    <span className="p-2  border-2 border-[#E0E0E0] ">
+                        <FiFilter />
+                    </span>
+                    <span className="p-2  border-2 border-[#E0E0E0] ">
+                        <BsQuestionCircle />
+                    </span>
+                </div>
+            </div>
+            <div className='w-full mx-4   max-md:hidden'>
+                <table className=" w-[97.5%]  bg-white">
+                    <thead className=' w-auto' style={{ height: "50px", borderBottom: "2px solid #E0E0E0" }}>
+                        <tr className=' flex w-auto mb-4  text-[#828282] '>
+                            <th className='p-4 w-1/6'>Name</th>
+                            <th className='p-4 w-1/6'>Diagnosis</th>
+                            <th className='p-4 w-1/6'>Status</th>
+                            <th className='p-4 w-1/6'>Last Appointment</th>
+                            <th className='p-4 w-1/6'>Next Appointment</th>
+                            <th className='p-4 w-1/6'>Option</th>
+                        </tr>
+                    </thead>
+
+                    <tbody className='bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full' style={{ height: "54vh" }}>
+                        {
+                            data.map((item) => {
+                                if (item.Status === 'Recovered') {
+                                    var background = "bg-emerald-200 p-[5px] w-4/5 rounded-full";
+                                }
+                                else if (item.Status === 'Awaiting surgery') {
+                                    background = "bg-blue-200 p-[5px]  w-4/5 rounded-full";
+                                }
+                                else {
+                                    background = "bg-red-200 p-[5px] w-4/5 rounded-full";
+                                }
+                                return (
+
+                                    <tr className=' h-12 flex w-full mb-4 text-center ' key={item.Name}>
+                                        <td className='p-4 w-1/6'>{item.Name}</td>
+                                        <td className='p-4 w-1/6'>{item.Diagnosis}</td>
+                                        <td className='pt-4 w-1/6'>  <button type="button" className={background} >{item.Status} </button>  </td>
+                                        <td className='p-4 w-1/6'>{item['Last Appointment']}</td>
+                                        <td className='p-4 w-1/6'>{item['Next Appointment']}</td>
+                                        <td className='p-4 w-1/6 text-4xl flex items-center  justify-center'>{item.Option}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+            <div className="w-full md:hidden overflow-x-scroll" style={{height:"70vh"}}>
+                {data.map((item)=>{
+                     if (item.Status === 'Recovered') {
+                        var background = "bg-emerald-200 p-[2px] w-4/5 rounded-full";
+                    }
+                    else if (item.Status === 'Awaiting surgery') {
+                        background = "bg-blue-200 p-[2px]  w-4/5 rounded-full";
+                    }
+                    else {
+                        background = "bg-red-200 p-[2px] w-4/5 rounded-full";
+                    }
+                    return(
+                        <div class="grid grid-cols-2 p-2 gap-4 mx-4 my-2 bg-white">
+                        <div class="grid grid-flow-row auto-rows-max">
+                            <div className='whitespace nowrap'>Name</div>
+                            <div className='whitespace nowrap max-w-full'>Diagnosis</div>
+                            <div className='whitespace nowrap max-w-full'>Status</div>
+                            <div className='whitespace nowrap max-w-full'>Last Appointment</div>
+                            <div className='whitespace nowrap max-w-full'>Next Appointment</div>
+                            <div className='whitespace nowrap max-w-full'>Option</div>
+                        </div>
+                        <div class="grid grid-flow-row auto-rows-max">
+                            <div>{item.Name}</div>
+                            <div>{item.Diagnosis}</div>
+                            <div><button type="button" className={background}>{item.Status}</button> </div>
+                            <div>{item['Last Appointment']}</div>
+                            <div>{item['Next Appointment']}</div>
+                            <div>{item.Option}</div>
+                        </div>
+    
+                       
+                    </div>
+                    )
+                })}
+
+
+            </div >
+            <div className="flex items-center gap-4 float-end mx-4 my-2">
+                <Button
+                    variant="text"
+                    className="flex items-center gap-2 rounded-full  "
+                    onClick={prev}
+                    disabled={active === 1}
+                >
+                    <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" /> Prev
+                </Button>
+                <div className="flex items-center gap-2">
+                    <IconButton {...getItemProps(1)}>1</IconButton>
+                    <IconButton {...getItemProps(2)}>2</IconButton>
+                    <IconButton {...getItemProps(3)}>3</IconButton>
+                    <IconButton {...getItemProps(4)}>4</IconButton>
+                    <IconButton {...getItemProps(5)}>5</IconButton>
+                </div>
+                <Button
+                    variant="text"
+                    className="flex items-center gap-2 rounded-full"
+                    onClick={next}
+                    disabled={active === 5}
+                >
+                    Next
+                    <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
+
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default Patients
